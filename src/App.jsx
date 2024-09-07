6604aec0ae5792b477c2579f6e5c09a9a68d06a0
Gaurav Shamra <sharmagauravxo@gmail.com>
2024-09-07 20:28:58 +0530

    final push aptos thing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './App.css'
 import PaymentComponent from './PaymentComponent'
+import Gemini_chat from './Gemini_chat'
+// import HealthAI from './Healthai'
 
 function App() {
   return (
     <div className="App">
-      <h1>Aptos Payment Gateway</h1>
+      {/* <h1>Aptos Payment Gateway</h1> */}
       <PaymentComponent />
+      {/* <Gemini_chat/> */}
+      {/* <HealthAI/> */}
     </div>
   )
 }
 
-export default App
+export default App;
